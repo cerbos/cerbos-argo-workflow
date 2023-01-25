229b5b72d1c546b421569b7762b0a3a28a6ecd5e
Alex Olivier <alex@alexolivier.me>
2023-01-25 09:51:34 +0000

    new tests
    Signed-off-by: Alex Olivier <alex@alexolivier.me>

--- demo-app/server.js
+++ demo-app/server.js
@@ -6,13 +6,43 @@ const cerbos = new Cerbos.GRPC(process.env.CERBOS_HOST, { tls: false });
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const sample = {
+  principal: {
+    id: "sally",
+    roles: ["USER"],
+    attributes: {
+      department: "SALES",
+      region: "EMEA",
+    },
+  },
+  resource: {
+    kind: "expense",
+    id: "expense1",
+    policyVersion: "default",
+    attributes: {
+      ownerId: "sally",
+      createdAt: "2021-10-01T10:00:00.021-05:00",
+      vendor: "Flux Water Gear",
+      region: "EMEA",
+      amount: 500,
+      status: "OPEN",
+    },
+  },
+  actions: ["approve", "create", "delete", "update", "view", "view:approver"],
+};
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await cerbos.checkResource(sample);
+    res.json(result);
+  } catch (e) {
+    res.status(402).json(e);
+  }
 });
 
 app.post("/cerbos", async (req, res) => {
   try {
-    const result = await cerbos.checkResources(req);
+    const result = await cerbos.checkResource(req);
     res.json(result);
   } catch (e) {
     res.status(402).json(e);
